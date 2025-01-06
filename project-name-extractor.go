23e6aa89bb3eae2e5ac0cd75d111fd86afc487af
Slava <verdark768@yandex.ru>
2025-01-06 21:47:47 +0300

    wip

--- project-name-extractor.go
+++ project-name-extractor.go
@@ -1,6 +1,7 @@
 package go_chef_code_gen_utils
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -18,17 +19,17 @@ func GetPath(destination string) string {
 	return destination
 }
 
-func GetProjectName(projectPath string) string {
+func GetProjectName(projectPath string) (string, error) {
 	projectPath = GetPath(projectPath)
 	goModPath := filepath.Join(projectPath, "go.mod")
 	f, err := os.ReadFile(goModPath)
 	if err != nil {
-		panic(fmt.Sprintf("cannot read go.mod in folder %s %v", projectPath, err))
+		return "", errors.New(fmt.Sprintf("cannot read go.mod in folder %s %v", projectPath, err))
 	}
 	splittedFile := strings.Split(string(f), "\n")
 	if len(splittedFile) == 0 {
-		panic(fmt.Sprintf("empty go.mod file in folder %s", projectPath))
+		return "", errors.New(fmt.Sprintf("empty go.mod file in folder %s", projectPath))
 	}
 	fm := strings.Replace(splittedFile[0], "module ", "", 1)
-	return strings.Replace(fm, "\r", "", 1)
+	return strings.Replace(fm, "\r", "", 1), nil
 }
